c10010e2256d9cb8bd11cc22f50d41603eb9bd6b
João Freitas <joao.mag.freitas@gmail.com>
2026-06-22 22:51:07 +0100

    chore: update education

--- render.sh
+++ render.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rendercv render resume.yml --design.theme engineeringresumes -pdf resume.pdf -nopng -nomd -nohtml
+rendercv render resume.yml -pdf resume.pdf -nopng -nomd -nohtml
